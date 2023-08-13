20debf2939ef33992710532f3736bc653a11b648
Amit <amiamit576@gmail.com>
2023-08-13 12:41:10 +0530

    react2

---
+++ src/components/Video.js
@@ -0,0 +1,22 @@
+import './Video.css';
+
+function Video({ title, channel = "Coder Dost", views, time,verified }) {
+  
+  return (
+      <>
+      <div className='container'>
+      <div className="pic">
+      <img src="http://placeimg.com/160/90/3" alt="Katherine Johnson" />
+      </div>
+        <div className="title">{title}</div>
+      {/* short circuiting */}
+      <div className="channel">{channel} {verified && '✅'} </div>
+      <div className="views">
+        {views} views <span>.</span> {time}
+      </div>
+      </div>
+      </>
+  );
+}
+
+export default Video;
